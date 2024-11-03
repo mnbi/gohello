e704bad40296b48ee6a8ee6fd8fa756bbeaa69b8
mnbi <mnbi@users.noreply.github.com>
2024-11-03 18:08:20 +0900

    use flag package to parse command line options

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var printVersion bool
+var printUsage bool
 
 func main() {
+	flag.BoolVar(&printVersion, "v", false, "print version")
+	flag.BoolVar(&printUsage, "h", false, "print usage")
+	flag.Parse()
+
+	if printVersion {
+		version()
+		os.Exit(1)
+	}
+
+	if printUsage {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	fmt.Println("Hello, world!")
+}
+
+func version() {
+	fmt.Println("gohello version 0.1.0")
 }
